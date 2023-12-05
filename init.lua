8350995bd69430cfea989475057a198e6e079e86
lecav30 <lecav30@outlook.com>
2023-12-04 20:56:54 -0500

    so errors with vscode neovim

--- init.lua
+++ init.lua
@@ -1,12 +1,3 @@
 vim.g.mapleader = " "
-if vim.g.vscode then
-  require("vscode")
-else
-	require("keymappings")
-	require("settings")
-end
-
--- Mapping helper
--- local mapper = function(mode, key, result)
---   vim.api.nvim_set_keymap(mode, key, result, {noremap = true, silent = true})
--- end
+require("keymappings")
+require("settings")
